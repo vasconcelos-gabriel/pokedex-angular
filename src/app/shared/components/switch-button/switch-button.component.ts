b5fbca2c324e82348948acc7e30241120094d5d9
vasconcelos-gabriel <vasconcelos.crvg@gmail.com>
2024-11-01 00:21:25 -0300

    fix: correcçao funcionmento do funcionamento do botao de swithc entre dark e light theme

--- src/app/shared/components/switch-button/switch-button.component.ts
+++ src/app/shared/components/switch-button/switch-button.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-switch-button',
   templateUrl: './switch-button.component.html',
-  styleUrl: './switch-button.component.scss',
+  styleUrls: ['./switch-button.component.scss'],
 })
 export class SwitchButtonComponent implements OnInit {
   isDarkTheme = false;
